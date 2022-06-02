b4538c30f8fca50886e68024884048dadab81655
uni0415 <rudd1242@naver.com>
2022-06-02 14:27:04 +0900

    burgerking_clone project

---
+++ burgerking/src/main/java/com/springboot/burgerking/BurgerkingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.burgerking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerkingApplication.class, args);
+	}
+
+}
---
+++ burgerking/src/test/java/com/springboot/burgerking/BurgerkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.burgerking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
