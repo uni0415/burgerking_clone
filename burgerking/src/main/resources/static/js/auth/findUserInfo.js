40e240ce546966efbabbcd0be2b9dbeb4bd4fe98
songsaemi <songsaemiye@naver.com>
2022-06-08 18:02:56 +0900

    2022.06.08 (수) 로그인 페이지 수정 아이디/비밀번호 찾기 페이지 css/js 구현 성공

---
+++ burgerking/src/main/resources/static/js/auth/findUserInfo.js
@@ -0,0 +1,45 @@
+const find_username_button = document.querySelector(".order-button-left");
+const find_password_button = document.querySelector(".order-button-right");
+const find_username_form = document.querySelector(".find-username-form");
+const find_password_form = document.querySelector(".find-password-form");
+const input_items = document.querySelectorAll(".userinfo-detail-box input");
+const cancellation_button = document.querySelectorAll(".cancellation-button");
+
+find_username_button.onclick = () => {
+    find_password_button.classList.remove("on");
+    find_username_button.classList.add("on");
+    find_username_form.classList.add("on");
+    find_password_form.classList.remove("on");
+}
+
+find_password_button.onclick = () => {
+    find_password_button.classList.add("on");
+    find_username_button.classList.remove("on");
+    find_username_form.classList.remove("on");
+    find_password_form.classList.add("on");
+}
+
+
+
+for (let i = 0; i < input_items.length; i++) {
+    input_items[i].onkeydown = () => {
+        if (input_items[i].value.length > 0) {
+            cancellation_button[i].classList.add("active");
+        }
+    }
+
+    input_items[i].onkeyup = (e) => {
+        if (e.keyCode == 8) {
+            if (input_items[i].value == 0) {
+                cancellation_button[i].classList.remove("active");
+            }
+        }
+    }
+    cancellation_button[i].onclick = () => {
+        input_items[i].value = null;
+        cancellation_button[i].classList.remove("active");
+
+    }
+}
+
+
