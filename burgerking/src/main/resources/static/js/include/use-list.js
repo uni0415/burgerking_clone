655a6284dcc74f8a205913b267ffffbb835ff226
ITPS <ITPS@gmail.com>
2022-06-10 22:19:56 +0900

    0610 비회원주문 페이지 마무리 메인페이지 슬라이드 추가 중

--- burgerking/src/main/resources/static/js/include/use-list.js
+++ burgerking/src/main/resources/static/js/include/use-list.js
@@ -1,24 +1,17 @@
 const btn_use = document.querySelector(".btn-use");
-const active_content_box = document.querySelector(".active-content-box");
-const content_position_box = document.querySelector(".content-position-box");
-
+const use_contents = document.querySelector(".use-contents");
 const input_items = document.querySelectorAll(".info-list input");
-const btn_delete = document.querySelectorAll(".btn-delete");
+const btn_delete = document.querySelector(".btn-delete");
 
 const btn_view = document.querySelectorAll(".btn-view");
+const password_input = document.querySelectorAll(".password-input");
 
 
 btn_use.onclick = () => {
-    active_content_box.classList.toggle("open");
-    content_position_box.classList.toggle("open");
+    use_contents.classList.toggle("open");
 }
 
 for (let i = 0; i < input_items.length; i++) {
-    input_items[i].onkeydown = () => {
-        if (input_items[i].value.length > 0) {
-            btn_delete[i].classList.add("active");
-        }
-    }
 
     input_items[i].onkeyup = (e) => {
         if (e.keyCode == 8) {
@@ -27,15 +20,27 @@ for (let i = 0; i < input_items.length; i++) {
             }
         }
     }
-    btn_delete[i].onclick = () => {
-        input_items[i].value = null;
-        btn_delete[i].classList.remove("active");
-
-    }
-
     btn_view[i].onclick = () => {
         btn_view[i].classList.toggle("on");
+        if (btn_view[i].classList.contains("on")) {
+            password_input[i].type = 'text';
+        } else {
+            password_input[i].type = 'password';
+        }
     }
 }
+
+input_items[0].onkeydown = () => {
+    if (input_items[0].value.length > 0) {
+        btn_delete.classList.add("active");
+    }
+}
+btn_delete.onclick = () => {
+    input_items[0].value = null;
+    btn_delete.classList.remove("active");
+
+}
+
+
 
 
