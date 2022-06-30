15f279745a0b94d47efce10482a3a35c9c1d55f6
uni0415 <rudd1242@naver.com>
2022-06-30 16:43:14 +0900

    2022. 06. 30
      drink 모달 창 side모달창과 같이 변경 메뉴들 총합 금액 띄워야함

--- burgerking/src/main/resources/static/js/delivery/delivery_cart.js
+++ burgerking/src/main/resources/static/js/delivery/delivery_cart.js
@@ -2,16 +2,13 @@ let menu_id = sessionStorage.getItem("menu_id");
 let side_menu_id = sessionStorage.getItem("side_menu_id");
 let drink_menu_id = sessionStorage.getItem("drink_menu_id");
 let size = sessionStorage.getItem("size");
-console.log(menu_id, size, side_menu_id, drink_menu_id);
 
 const cart_list_box = document.querySelector(".cart-list-box");
 const calc_total_price_tag = document.querySelector(".calc-total-price");
 
 const add_menu_to_card_button = document.querySelector(".menu-add-button");
 
-let total_price = 0;
-let calc_total_price = 0;
-let menu_count = 1;
+
 
 let cart_list_json;
 
@@ -58,10 +55,17 @@ function getCartListFromSession() {
 		},
 		dataType: "json",
 		success: function(menu_data_list) {
+		console.log("menu_data_list");
+		console.log(menu_data_list);
 			menu_data_list = devideMenuData(cart_list, menu_data_list);
 			for (let i = 0; i < menu_data_list.length; i++) {
-				const cart_menu_tag = makeCartMenuTag(menu_data_list[i]);
+				let total_price = 0;
+				let calc_total_price = 0;
+				let menu_count = 1;
+
+				const cart_menu_tag = makeCartMenuTag(menu_data_list[i], total_price);
 				cart_list_box.appendChild(cart_menu_tag);
+				const main_menu_price = cart_menu_tag.querySelector(".price");
 
 				const total_price_tag = cart_menu_tag.querySelector(".total-price");
 
@@ -69,13 +73,29 @@ function getCartListFromSession() {
 				const reduce_menu_count_button = cart_menu_tag.querySelector(".num-set > .btn-minus");
 				const add_menu_count_button = cart_menu_tag.querySelector(".num-set > .btn-plus");
 				additionalList(menu_data_list[i], cart_menu_tag);
-				
+
+				const ingredient_add_price = cart_menu_tag.querySelector(".ingredient-add-price");
+				const side_add_price = cart_menu_tag.querySelector(".side-add-price");
+				const drink_add_price = cart_menu_tag.querySelector(".drink-add-price");
+
 				const ingredient_change_button = cart_menu_tag.querySelector("#ingredient-change-button");
 				const side_change_button = cart_menu_tag.querySelector("#side-change-button");
 				const drink_change_button = cart_menu_tag.querySelector("#drink-change-button");
+				
+				menu_price = main_menu_price.textContent;
+				side_price = side_add_price.textContent;
+				drink_price = drink_add_price.textContent;
+								
 				popSideMenuModal(side_change_button, menu_data_list[i].menu, cart_menu_tag, i);
 				popDrinkMenuModal(drink_change_button, menu_data_list[i].menu, cart_menu_tag, i);
-				//setOrderList(menu_data_list);
+
+				reduce_menu_count_button.onclick = () => {
+					reduceMenuCount(total_price, calc_total_price, menu_count, menu_count_tag, total_price_tag);
+				}
+
+				add_menu_count_button.onclick = () => {
+					addMenuCount(total_price, calc_total_price, menu_count, menu_count_tag, total_price_tag);
+				}
 			}
 		},
 		error: function(xhr, status) {
@@ -91,10 +111,10 @@ function additionalList(menu_data, cart_menu_tag) {
 	if (menu_data.menu.id < 4) {
 		const ingredient_list_tag = makeIngredientTag(menu_data.menu);
 		const drink_menu_tag = makeDrinkMenuTag(menu_data.drink_menu, menu_data.drink_menu.set_size);
-
+		
 		set_menu_detail.appendChild(ingredient_list_tag);
 		set_menu_detail.appendChild(drink_menu_tag);
-		
+
 
 	} else if (menu_data.menu.set_size == 0) {
 		const ingredient_list_tag = makeIngredientTag(menu_data.menu);
@@ -108,6 +128,7 @@ function additionalList(menu_data, cart_menu_tag) {
 		set_menu_detail.appendChild(ingredient_list_tag);
 		set_menu_detail.appendChild(side_menu_tag);
 		set_menu_detail.appendChild(drink_menu_tag);
+
 	}
 }
 
@@ -157,7 +178,7 @@ function makeIngredientTag(menu) {
                     <span>${menu.ingredient_list[i].name}</span>
                     <strong>
                         +
-                        <span class="add-price">${menu.ingredient_list[i].price}</span>
+                        <span class="ingredient-add-price">${menu.ingredient_list[i].price}</span>
                         <span>원</span>
                     </strong>
                 </div>
@@ -181,7 +202,7 @@ function makeSideMenuTag(side_menu, set_size) {
 	            <span class="side-menu-name">${side_menu.name}</span>
 	            <strong>
 	                +
-	                <span class="add-price">${set_size == 1 ? side_menu.set_add_price : side_menu.large_add_price}</span>
+	                <span class="side-add-price">${set_size == 1 ? side_menu.set_add_price : side_menu.large_add_price}</span>
 	                <span>원</span>
 	            </strong>
 	        </div>
@@ -200,10 +221,10 @@ function makeDrinkMenuTag(drink_menu, set_size) {
         <span>음료</span>
         <div class="additional-box">
             <div class="side-menu-list">
-                <span>${drink_menu.name}</span>
+                <span class="drink-menu-name">${drink_menu.name}</span>
                 <strong>
                     +
-                    <span class="add-price">${set_size == 1 ? drink_menu.set_add_price : drink_menu.large_add_price}</span>
+                    <span class="drink-add-price">${set_size == 1 ? drink_menu.set_add_price : drink_menu.large_add_price}</span>
                     <span>원</span>
                 </strong>
             </div>
@@ -216,6 +237,7 @@ function makeDrinkMenuTag(drink_menu, set_size) {
 }
 
 function makeCartMenuTag(menu_data) {
+	console.log(menu_data);
 	const li = document.createElement("li");
 	li.classList.add("menu-list");
 	li.innerHTML = `
@@ -274,8 +296,6 @@ function makeCartMenuTag(menu_data) {
 }
 
 
-/*reduce_menu_count_button.onclick = reduceMenuCount;
-add_menu_count_button.onclick = addMenuCount;*/
 
 add_menu_to_card_button.onclick = () => {
 	const cart_list = sessionStorage.getItem("cart_list") != null ? JSON.parse(sessionStorage.getItem("cart_list")) : new Array();
@@ -292,76 +312,34 @@ add_menu_to_card_button.onclick = () => {
 
 
 
-
-
-function setOrderList(menu_data_list) {
-	for (let i = 0; i < menu_data_list.length; i++) {
-
-		console.log("setOrderList: ");
-		console.log(menu_data_list);
-		setSideMenuList(menu_data_list[1]);
-		setSideDrinkList(menu_data_list[2]);
-		menu_count_tag.value = menu_count;
-		calcTotalPrice();
-	}
-}
-
-function addMenuCount() {
+function addMenuCount(total_price, calc_total_price, menu_count, menu_count_tag, total_price_tag) {
 	menu_count_tag.value = ++menu_count;
-	calcTotalPrice();
+	calcTotalPrice(total_price, calc_total_price, menu_count, total_price_tag);
 }
 
-function reduceMenuCount() {
+function reduceMenuCount(total_price, calc_total_price, menu_count, menu_count_tag, total_price_tag) {
 	if (menu_count == 1) return;
 	menu_count_tag.value = --menu_count;
-	calcTotalPrice();
+	calcTotalPrice(total_price, calc_total_price, total_price_tag);
 }
 
-/*function setSideMenuList(data) {
-	if (data.length == 1) {
-		return;
-	} else if (data.length == 2) {
-		return;
-	} else if (data.length == 3) {
-		let side_add = size == 1 ? data[1].set_add_price : data[1].large_add_price;
-		side_menu_list[0].innerText = data[1].name;
-		add_price[0].innerText = side_add;
-		total_price += Number(side_add);
-	}
+
+
+function calcTotalPrice(total_price, calc_total_price, menu_count, total_price_tag) {
+	console.log(menu_count);
+	console.log(total_price);
+	total_price_tag.innerText = Number(total_price * menu_count).toLocaleString('ko-KR');
+	calcLastPrice(total_price_tag, calc_total_price);
 }
 
-function setSideDrinkList(data) {
-	if (data.length == 1) {
-		return;
-	} else if (data.length == 2) {
-		console.log(data);
-		let drink_add = size == 1 ? data[1].set_add_price : data[1].large_add_price;
-		side_menu_list[2].innerText = data[1].name;
-		add_price[2].innerText = drink_add;
-		side_menu_list[3].innerText = data[1].name;
-		add_price[3].innerText = drink_add;
-		total_price += Number(drink_add);
-	} else if (data.length == 3) {
-		let drink_add = size == 1 ? data[2].set_add_price : data[2].large_add_price;
-		side_menu_list[1].innerText = data[2].name;
-		add_price[1].innerText = drink_add;
-		total_price += Number(drink_add);
-	}
-}*/
-
-function calcTotalPrice() {
-	total_sum.innerText = Number(total_price * menu_count).toLocaleString('ko-KR');
-	calcLastPrice();
-}
-
-function calcLastPrice() {
-	total_sum.innerText = total_price.toLocaleString('ko-KR');
+function calcLastPrice(total_price, calc_total_price, total_price_tag) {
+	total_price_tag.innerText = total_price.toLocaleString('ko-KR');
 	if (total_price * menu_count > 17000) {
 		calc_total_price = total_price * menu_count - 5000;
-		calc_total_sum.innerText = calc_total_price.toLocaleString('ko-KR');
+		calc_total_price_tag.innerText = calc_total_price.toLocaleString('ko-KR');
 	} else {
 		calc_total_price = total_price * menu_count;
-		calc_total_sum.innerText = calc_total_price.toLocaleString('ko-KR');
+		calc_total_price_tag.innerText = calc_total_price.toLocaleString('ko-KR');
 	}
 }
 
--- burgerking/src/main/resources/static/js/delivery/menu-change-modal.js
+++ burgerking/src/main/resources/static/js/delivery/menu-change-modal.js
@@ -9,7 +9,11 @@ const btn_top = document.querySelector(".btn-top");
 
 
 
-
+function removeAllChild(tag) {
+	while(tag.hasChildNodes()) {
+		tag.removeChild(tag.firstChild);
+	}
+}
 
 function popSideMenuModal(side_change_button, menu_data_list, cart_menu_tag, index) {
 	let side_menu_data_list;
@@ -17,8 +21,6 @@ function popSideMenuModal(side_change_button, menu_data_list, cart_menu_tag, ind
 		modal_pop_wrap.classList.add("on");
 		body.style = "overflow: hidden";
 		side_modal_box.classList.add("on");
-		console.log("side: ");
-		console.log(menu_data_list);
 
 		$.ajax({
 			type: "get",
@@ -29,7 +31,6 @@ function popSideMenuModal(side_change_button, menu_data_list, cart_menu_tag, ind
 				side_menu_data_list = JSON.parse(data);
 				loadChangeSideMenu(side_menu_data_list, menu_data_list, index);
 				sideMenuChoice(side_menu_data_list, menu_data_list, cart_menu_tag)
-				console.log(sessionStorage.getItem("side_menu_id"));
 			}
 		})
 	}
@@ -43,11 +44,11 @@ function popSideMenuModal(side_change_button, menu_data_list, cart_menu_tag, ind
 
 function sideMenuChoice(side_menu_data_list, menu_data_list, cart_menu_tag) {
 	const side_menu_name = cart_menu_tag.querySelector(".side-menu-name");
-	const add_price = cart_menu_tag.querySelector(".add-price");
+	const side_add_price = cart_menu_tag.querySelector(".side-add-price");
 	choice_button[0].onclick = () => {
 		selected_side_menu = side_menu_data_list[side_menu_data_list.findIndex(e => e.id == side_menu_id)];
 		side_menu_name.innerText = selected_side_menu.name;
-		add_price.innerText = menu_data_list.set_size == 1 ? selected_side_menu.set_add_price : selected_side_menu.large_add_price;
+		side_add_price.innerText = menu_data_list.set_size == 1 ? selected_side_menu.set_add_price : selected_side_menu.large_add_price;
 		modal_close_button[0].click();
 	}
 }
@@ -56,18 +57,17 @@ function loadChangeSideMenu(side_menu_data_list, menu_data_list, index) {
 
 	cart_list = JSON.parse(sessionStorage.getItem("cart_list"));
 	const menu_change = document.querySelector(".menu-change");
+	removeAllChild(menu_change);
 	for (let i = 0; i < side_menu_data_list.length; i++) {
 		const tag = makeSideTag(side_menu_data_list[i], menu_data_list);
 		menu_change.appendChild(tag);
 		tag.onclick = () => {
 			sessionStorage.setItem("side_menu_id", side_menu_data_list[i].id);
 			side_menu_id = sessionStorage.getItem("side_menu_id");
-			for (let i = 0; i < cart_list.length; i++) {
+			for (let j = 0; j < cart_list.length; j++) {
 				cart_list[index].side_menu_id = side_menu_id;
 				sessionStorage.setItem("cart_list", JSON.stringify(cart_list));
 			}
-			console.log(cart_list);
-
 		}
 		menu_change.appendChild(tag);
 	}
@@ -75,7 +75,7 @@ function loadChangeSideMenu(side_menu_data_list, menu_data_list, index) {
 }
 
 function makeSideTag(side_menu_data, menu_data_list) {
-	const add_price = menu_data_list.set_size == 1 ? side_menu_data.set_add_price : side_menu_data.large_add_price;
+	const side_add_price = menu_data_list.set_size == 1 ? side_menu_data.set_add_price : side_menu_data.large_add_price;
 	const li = document.createElement("li");
 	li.className = "";
 	li.innerHTML = `
@@ -88,7 +88,7 @@ function makeSideTag(side_menu_data, menu_data_list) {
                     <span>${side_menu_data.name} 교환</span>
                 </p>
                 <p class="menu-change-price">
-                    <span>+${add_price}원</span>
+                    <span>+${side_add_price}원</span>
                 </p>
             </div>
             <label class="list-check">
@@ -110,28 +110,17 @@ function popDrinkMenuModal(drink_change_button, menu_data_list, cart_menu_tag, i
 		body.style = "overflow: hidden";
 		drink_modal_box.classList.add("on");
 
-		$.ajax({
-			type: "get",
-			dataType: "text",
-			url: `/api/v1/delivery/side/${menu_data_list.set_size}`,
-			async: false,
-			success: function(data) {
-				side_menu_data_list = JSON.parse(data);
-				loadChangeSideMenu(side_menu_data_list, menu_data_list, index);
-				sideMenuChoice(side_menu_data_list, menu_data_list, cart_menu_tag)
-				console.log(sessionStorage.getItem("side_menu_id"));
-			}
-		})
-
 
 		$.ajax({
 			type: "get",
 			dateType: "text",
 			async: false,
-			url: `/api/v1/delivery/drink/${menu_data_list.size}`,
+			url: `/api/v1/delivery/drink/${menu_data_list.set_size}`,
 			success: function(data) {
-				drink_menu_data_list = JSON.parse(data);
+				drink_menu_data_list = JSON.stringify(data);
+				drink_menu_data_list = JSON.parse(drink_menu_data_list);
 				loadChangeDrinkMenu(drink_menu_data_list, menu_data_list, index);
+				drinkMenuChoice(drink_menu_data_list, menu_data_list, cart_menu_tag);
 			}
 		})
 	}
@@ -142,43 +131,64 @@ function popDrinkMenuModal(drink_change_button, menu_data_list, cart_menu_tag, i
 	}
 }
 
+
 function loadChangeDrinkMenu(drink_menu_data_list, menu_data_list, index) {
-	let str = ``;
-	let add_price = 0;
-	for (let i = 0; i < drink_menu_data.length; i++) {
-		set_size == 1 ? add_price = drink_menu_data[i].set_add_price : add_price = drink_menu_data[i].large_add_price;
-		str += `
-        <li>
-            <div class="drink-change-image-box">
-                <img src="${drink_menu_data[i].menu_images}" alt="">
-            </div>
-            <div class="drink-change-content-box">
-                <p class="drink-change-title">
-                    <span>${drink_menu_data[i].name} 교환</span>
-                </p>
-                <p class="drink-change-price">
-                    <span>+${add_price}원</span>
-                </p>
-            </div>
-            <label class="list-check">
-                <input type="radio" name="drink-option" value="${i}">
-                <span></span>
-            </label>
-        </li>
-        `
-	}
+	cart_list = JSON.parse(sessionStorage.getItem("cart_list"));
 	const drink_change = document.querySelector(".drink-change");
-	drink_change.innerHTML = str;
-	drinkMenuChoice(drink_menu_data);
+	removeAllChild(drink_change);
+	for (let i = 0; i < drink_menu_data_list.length; i++) {
+		const tag = makeDrinkTag(drink_menu_data_list[i], menu_data_list);
+		drink_change.appendChild(tag);
+		tag.onclick = () => {
+			sessionStorage.setItem("drink_menu_id", drink_menu_data_list[i].id);
+			drink_menu_id = sessionStorage.getItem("drink_menu_id");
+			console.log(drink_menu_id);
+			for (let j = 0; j < cart_list.length; j++) {
+				cart_list[index].drink_menu_id = drink_menu_id;
+				sessionStorage.setItem("cart_list", JSON.stringify(cart_list));
+			}
+		}
+		drink_change.appendChild(tag);
+
+	}
 }
+
+
+function makeDrinkTag(drink_menu_data, menu_data_list) {
+	const drink_add_price = menu_data_list.set_size == 1 ? drink_menu_data.set_add_price : drink_menu_data.large_add_price;
+	const li = document.createElement("li");
+	li.className = "";
+	li.innerHTML = `
+	<li>
+        <div class="drink-change-image-box">
+            <img src="${drink_menu_data.menu_images}" alt="">
+        </div>
+        <div class="drink-change-content-box">
+            <p class="drink-change-title">
+                <span>${drink_menu_data.name} 교환</span>
+            </p>
+            <p class="drink-change-price">
+                <span>+${drink_add_price}원</span>
+            </p>
+        </div>
+        <label class="list-check">
+            <input type="radio" name="drink-option" ${drink_menu_data.id == drink_menu_id ? 'checked' : ''}>
+            <span></span>
+        </label>
+    </li>
+        `
+	return li;
+}
+
+
 
 function drinkMenuChoice(drink_menu_data_list, menu_data_list, cart_menu_tag) {
 	const drink_menu_name = cart_menu_tag.querySelector(".drink-menu-name");
-	const add_price = cart_menu_tag.querySelector(".add-price");
+	const drink_add_price = cart_menu_tag.querySelector(".drink-add-price");
 	choice_button[1].onclick = () => {
 		selected_drink_menu = drink_menu_data_list[drink_menu_data_list.findIndex(e => e.id == drink_menu_id)];
 		drink_menu_name.innerText = selected_drink_menu.name;
-		add_price.innerText = menu_data_list.set_size == 1 ? selected_drink_menu.set_add_price : selected_drink_menu.large_add_price;
+		drink_add_price.innerText = menu_data_list.set_size == 1 ? selected_drink_menu.set_add_price : selected_drink_menu.large_add_price;
 		modal_close_button[1].click();
 	}
 }
